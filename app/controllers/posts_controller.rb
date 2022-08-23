2d96272658ec1a569276803471c642c78d8a1af0
supersakana <zacwilliamson@icloud.com>
2022-08-23 17:47:00 -0600

    updated for discord question

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,30 @@
 class PostsController < ApplicationController
-    
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build
+
+    if @post.save
+       redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def post_params
+      params.permit(:title, :body)
+    end
 end
