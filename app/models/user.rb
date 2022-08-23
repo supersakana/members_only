c16c4bf14f4dc6a642543cdcd0ce84530f79d57b
supersakana <zacwilliamson@icloud.com>
2022-08-23 09:15:24 -0600

    added basic sign up view. uesrs now can be added to the database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :posts
-  
+
+  validates :username, presence: true, length: { in: 3..20 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
