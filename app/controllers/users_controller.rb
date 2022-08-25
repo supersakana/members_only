10c36b2715f08926e3147e524af53ab11fdb55ca
supersakana <zacwilliamson@icloud.com>
2022-08-25 11:03:59 -0600

    when user signs in, redirectes to index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,6 @@ class UsersController < ApplicationController
       end
     end
 
-    def show
-      @user = User.find(params[:id])
-    end
-
     private
     def user_params
       params.require(:user).permit(:username, :password, :email)
